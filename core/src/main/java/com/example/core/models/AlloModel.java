cb981879bf543c5bd6b89337d72e159e1f80a650
yakub <yakub3k+git@gmail.com>
2026-03-23 16:21:29 +0100

    changed - Allo Sling model

--- core/src/main/java/com/example/core/models/AlloModel.java
+++ core/src/main/java/com/example/core/models/AlloModel.java
@@ -1,12 +1,14 @@
 package com.example.core.models;
 
 import com.adobe.cq.wcm.core.components.models.Page;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
 
 import javax.annotation.Resource;
 
 @Model(adaptables = Resource.class)
+@Exporter(name = "jackson", extensions = "json", selector = "model")
 public class AlloModel {
 
     @ScriptVariable
